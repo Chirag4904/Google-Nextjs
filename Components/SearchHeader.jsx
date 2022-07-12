7379e0e89d7054550623e11a42b6d07cf71b06e9
chiggy69 <chiragaggarwal69@gmail.com>
2022-07-12 15:13:50 +0530

    Search header ui and redirection added

---
+++ Components/SearchHeader.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { useRef } from "react";
+import { SearchIcon, MicrophoneIcon, XIcon } from "@heroicons/react/solid";
+import User from "./User";
+export default function SearchHeader() {
+	const router = useRouter();
+	const searchInputRef = useRef(null);
+	const handleSearch = (e) => {
+		e.preventDefault();
+		const searchTerm = searchInputRef.current.value;
+		if (!searchTerm.trim()) {
+			return;
+		}
+
+		router.push(`/search?term=${searchTerm.trim()}`);
+	};
+	return (
+		<header className="sticky top-0 bg-white">
+			<div className="flex w-full p-6 items-center">
+				<Image
+					onClick={() => router.push("/")}
+					width="120"
+					objectFit="cover"
+					height="40"
+					className="cursor-pointer"
+					src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png"
+					alt="Google Logo"
+				/>
+				<form className="flex border border-gray-200 rounded-full shadow-lg px-6 py-3 ml-10 mr-5 flex-grow max-w-3xl items-center">
+					<input
+						type="text"
+						defaultValue={router.query.term}
+						ref={searchInputRef}
+						className="w-full focus:outline-none"
+					/>
+					<XIcon
+						onClick={() => (searchInputRef.current.value = "")}
+						className="h-6 text-gray-500 cursor-pointer sm:mr-3"
+					/>
+					<MicrophoneIcon className="h-6 hidden sm:inline-flex text-blue-500 pl-4 border-l-2 mr-3 border-gray-300" />
+					<SearchIcon className="h-6 hidden sm:inline-flex text-blue-500" />
+					<button onClick={handleSearch} type="submit" hidden></button>
+				</form>
+				{/* One way to do it */}
+				{/* <div className="ml-auto whitespace-nowrap">
+					<User className="" />
+				</div> */}
+				<User className="ml-auto whitespace-nowrap" />
+			</div>
+		</header>
+	);
+}
--- Components/User.jsx
+++ Components/User.jsx
@@ -1,6 +1,6 @@
 import { useSession, signIn, signOut } from "next-auth/react";
 
-export default function User() {
+export default function User({ className }) {
 	// console.log(signIn);
 	const { data: session } = useSession();
 	if (session) {
@@ -10,7 +10,7 @@ export default function User() {
 					onClick={signOut}
 					src={session.user.image}
 					alt="user-image"
-					className="h-10 w-10 rounded-full hover:bg-gray-500 cursor-pointer p-1"
+					className={`h-10 w-10 rounded-full hover:bg-gray-500 cursor-pointer p-[3px] ${className}`}
 				/>
 			</>
 		);
@@ -19,7 +19,7 @@ export default function User() {
 		<>
 			<button
 				onClick={signIn}
-				className="bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md"
+				className={`bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md ${className}`}
 			>
 				Sign In
 			</button>
