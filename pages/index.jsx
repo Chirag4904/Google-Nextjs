da87b3b1b1b811c1e0d81e8bafbd60bc73e6f65f
chiggy69 <chiragaggarwal69@gmail.com>
2022-07-12 13:41:17 +0530

    Getting term from input and redirecting to the url/page

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,7 +3,24 @@ import Header from "../Components/Header";
 import Image from "next/image";
 import { SearchIcon, MicrophoneIcon } from "@heroicons/react/solid";
 import Footer from "../Components/Footer";
+import { useRouter } from "next/router";
+import { useRef } from "react";
 export default function Home() {
+	const router = useRouter();
+	//useRef hook to reference to input element
+	const searchInputRef = useRef(null);
+	//function to handle search
+	const handleSearch = (e) => {
+		//preventing default so it doesn't refresh the page
+		e.preventDefault();
+		const searchTerm = searchInputRef.current.value;
+		//trim will remove the unnecessary whitespace
+		if (!searchTerm.trim()) {
+			return;
+		}
+		//if search term is not empty, redirect to search page
+		router.push(`/search?term=${searchTerm.trim()}`);
+	};
 	return (
 		<div>
 			<Head>
@@ -26,11 +43,17 @@ export default function Home() {
 				/>
 				<div className="flex mt-5 w-full mx-auto border max-w-[90%] border-gray-200 hover:shadow-lg focus-within:shadow-lg px-5 py-3 rounded-full items-center sm:max-w-xl lg:max-w-2xl">
 					<SearchIcon className="h-5 text-gray-500 mr-3" />
-					<input type="text" className="flex-grow focus:outline-none" />
+					<input
+						ref={searchInputRef}
+						type="text"
+						className="flex-grow focus:outline-none"
+					/>
 					<MicrophoneIcon className="h-5" />
 				</div>
 				<div className="flex flex-col sm:flex-row w-[50%] space-y-2 mt-8 sm:space-y-0 sm:space-x-2 justify-center">
-					<button className="btn">Google Search</button>
+					<button onClick={handleSearch} className="btn">
+						Google Search
+					</button>
 					<button className="btn">I&apos;m Feeling Lucky</button>
 				</div>
 			</form>
