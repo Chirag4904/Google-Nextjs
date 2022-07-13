dd3eee2617b5bc987146f1bc33d50d0f475a8a96
chiggy69 <chiragaggarwal69@gmail.com>
2022-07-13 17:18:24 +0530

    complete result section

--- Components/SearchResults.jsx
+++ Components/SearchResults.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-
+//parser will be used for parsing the html in the paragraph and converting it to actual html
+import Parser from "html-react-parser";
 export default function SearchResults({ results }) {
 	return (
 		<div className="w-full mx-auto px-3 sm:pl-[5%] md:pl-[14%] lg:pl-52">
@@ -7,6 +8,24 @@ export default function SearchResults({ results }) {
 				About {results.searchInformation.formattedTotalResults} results (
 				{results.searchInformation.formattedSearchTime} seconds)
 			</p>
+			{results.items.map((result) => (
+				<div key={result.link} className="max-w-xl mb-8">
+					<div className="group">
+						<a className="text-sm truncate" href={result.link}>
+							{result.formattedUrl}
+						</a>
+						<a
+							className="group-hover:underline decoration-blue-800"
+							href={result.link}
+						>
+							<h2 className="truncate text-xl font-medium text-blue-800">
+								{result.title}
+							</h2>
+						</a>
+					</div>
+					<p className="text-gray-600">{Parser(result.htmlSnippet)}</p>
+				</div>
+			))}
 		</div>
 	);
 }
